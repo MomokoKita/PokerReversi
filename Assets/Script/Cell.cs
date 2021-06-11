31bcd66480a8021452d35383f7e0984e98356303
NICOTYAN <fumomoSan@outlook.jp>
2021-06-11 12:48:37 +0900

    置ける場所の色を変更する

--- Assets/Script/Cell.cs
+++ Assets/Script/Cell.cs
@@ -8,6 +8,7 @@ public enum CellState
     Black = 0,
     White = 1,
     None = -1,
+    PutNone = -2,
 }
 
 public class Cell : MonoBehaviour
@@ -54,29 +55,64 @@ public class Cell : MonoBehaviour
         if (_cellState == CellState.None)
         {
             p_image.color = Color.clear;
+            m_image.color = Color.green;
         }
         if (_cellState == CellState.White)
         {
             p_image.color = Color.white;
+            m_image.color = Color.green;
+
         }
         if (_cellState == CellState.Black)
         {
             p_image.color = Color.black;
+            m_image.color = Color.green;
+        }
+        if (_cellState == CellState.PutNone)
+        {
+            Debug.Log("おけるよ");
+            m_image.color = Color.yellow;
         }
     }
     public void Put()
     {
         reversi = GameObject.Find("Reversi");
-        if (this.CellState == CellState.None)
+        if (this.CellState == CellState.PutNone)
         {
-            ReverseAll();
-            if (!(this.CellState == CellState.None))
+            reversi.GetComponent<Reversi>().AllCheck();
+            
+            ReverseAll();            
+            if (!(this.CellState == CellState.None || this.CellState == CellState.PutNone))
             {
+                
                 reversi.GetComponent<Reversi>().TrunChenge();
+                
             } 
         }
 
     }
+
+    public void PutNoneCheck()
+    {
+        reversi = GameObject.Find("Reversi");
+        if (this.CellState == CellState.None)
+        {
+            PutAll();
+        }
+    }
+
+    public void PutAll()
+    {
+        reversi.GetComponent<Reversi>().PutCheck(row,column,1,0);  //右方向
+        reversi.GetComponent<Reversi>().PutCheck(row, column, -1, 0); //左方向
+        reversi.GetComponent<Reversi>().PutCheck(row, column, 0, -1); //上方向
+        reversi.GetComponent<Reversi>().PutCheck(row, column, 0, 1);  //下方向
+        reversi.GetComponent<Reversi>().PutCheck(row, column, 1, -1); //右上方向
+        reversi.GetComponent<Reversi>().PutCheck(row, column, -1, -1);//左上方向
+        reversi.GetComponent<Reversi>().PutCheck(row, column, 1, 1);  //右下方向
+        reversi.GetComponent<Reversi>().PutCheck(row, column, -1, 1); //左下方向
+    }
+
     void ReverseAll()
     {
         reversi.GetComponent<Reversi>().ReverseCheck(row, column, 1, 0);  //右方向
--- Assets/Script/Reversi.cs
+++ Assets/Script/Reversi.cs
@@ -69,46 +69,12 @@ public class Reversi : MonoBehaviour
         }
 
     }
-    public int Neighbour(int r, int c)
+
+    private void Update()
     {
-        var left = c - 1;
-        var right = c + 1;
-        var top = r - 1;
-        var bottom = r + 1;
-        int count = 0;
-
-        CellState flag = CellState.None;
-        if(trun)//白のターン
-        {
-            flag = CellState.Black;            
-        }
-        else//黒のターン
-        {
-            flag = CellState.White;
-        }
-
-        Debug.Log("フラグのステート"+flag);
-
-        if (top >= 0)
-        {
-            if (left >= 0 && _cells[top, left].CellState == flag ) { count++; }
-            if (_cells[top, c].CellState == flag ) { count++; }
-            if (right < _columns && _cells[top, right].CellState == flag) { count++; }
-        }
-
-        if (left >= 0 && _cells[r, left].CellState == flag && left >= 0 ) { count++; }
-        if (right < _columns && _cells[r, right].CellState == flag ) { count++; }
-        if (bottom < _rows)
-        {
-            if (left >= 0 && _cells[bottom, left].CellState == flag ) { count++; }
-            if (_cells[bottom, c].CellState == flag ) { count++; }
-            if (right < _columns && _cells[bottom, right].CellState == flag ) { count++; }
-        }
-
-
-        return count;
+        NoneReset();
+        AllCheck();
     }
-
 
     public void ReverseCheck(int r,int c,int directionR, int directionC)
     {
@@ -119,6 +85,7 @@ public class Reversi : MonoBehaviour
         {
             return;
         }
+        
         CellState flag = CellState.None;
         if (!trun)//黒のターン
         {
@@ -142,12 +109,10 @@ public class Reversi : MonoBehaviour
             //自分の駒だった場合
             if (_cells[r_check, c_check].CellState == flag)
             {
-                Debug.Log("r_check"+r_check+"c_check"+c_check);
                 //ひっくり返す
                 int r_check2 = r + directionR, c_check2 = c + directionC;
                 while (!(r_check2 == r_check && c_check2 == c_check))
                 {
-                    Debug.Log("While →　横が" + c_check2 + "縦が" + r_check2);
                     _cells[r_check2, c_check2].CellState = flag;
                     r_check2 += directionR;
                     c_check2 += directionC;           
@@ -157,7 +122,61 @@ public class Reversi : MonoBehaviour
 
             }
             //空欄だった場合
-            else if (_cells[r_check, c_check].CellState == CellState.None)
+            else if (_cells[r_check, c_check].CellState == CellState.None || _cells[r_check, c_check].CellState == CellState.PutNone)
+            {
+                //挟んでいないので処理を終える
+                break;
+            }
+
+            //確認座標を次に進める
+            r_check += directionR;
+            c_check += directionC;
+        }
+    }
+
+    public void PutCheck(int r, int c, int directionR, int directionC)
+    {
+
+        //確認する座標
+        int r_check = r + directionR;
+        int c_check = c + directionC;
+        if (r_check < 0 || r_check >= _rows || c_check < 0 || c_check >= _columns)
+        {
+            return;
+        }
+        if (_cells[r, c].CellState == CellState.Black || _cells[r, c].CellState == CellState.White)
+        {
+            return;
+        }
+
+        CellState flag = CellState.None;
+        if (!trun)//黒のターン
+        {
+            flag = CellState.Black;
+
+        }
+        else if (trun) //白のターン
+        {
+            flag = CellState.White;
+        }
+        if (_cells[r_check, c_check].CellState == flag)
+        {
+            return;
+        }
+        while (r < _rows && r >= 0 && c < _columns && c >= 0)
+        {
+            if (r_check < 0 || r_check >= _rows || c_check < 0 || c_check >= _columns)
+            {
+                return;
+            }
+            //自分の駒だった場合
+            if (_cells[r_check, c_check].CellState == flag)
+            {
+                _cells[r, c].CellState = CellState.PutNone;
+                break;
+            }
+            //空欄だった場合
+            else if (_cells[r_check, c_check].CellState == CellState.None || _cells[r_check, c_check].CellState == CellState.PutNone)
             {
                 //挟んでいないので処理を終える
                 break;
@@ -172,7 +191,6 @@ public class Reversi : MonoBehaviour
 
     public void TrunChenge()
     {
-        Debug.Log("ターンの切り替え");
         if (trun)
         {
             trunText.text = "黒のターン";
@@ -196,6 +214,7 @@ public class Reversi : MonoBehaviour
         {
             for (int c = 0; c < _columns; c++)
             {
+                _cells[r, c].PutNoneCheck();
                 if(_cells[r,c].CellState == CellState.Black)
                 {
                     blackCount++;
@@ -208,8 +227,19 @@ public class Reversi : MonoBehaviour
         }
         blackText.text = "黒：" + blackCount;
         whiteText.text = "白：" + whiteCount;
-
-
+    }
+    public void NoneReset()
+    {
+        for (int r = 0; r < _rows; r++)
+        {
+            for (int c = 0; c < _columns; c++)
+            {
+                if (_cells[r,c].CellState == CellState.PutNone)
+                {
+                    _cells[r, c].CellState = CellState.None;
+                }
+            }
+        }
     }
 
     /// <summary>
